f572a910c003c5786d9a880758a40afa51b8bfdb
Joe <joepatmckenna@gmail.com>
2018-08-25 07:33:07 -0400

    0.0.12

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-(rm -rf dist/)
-(python increment_version_number.py)
-(python setup.py sdist)
-(twine upload dist/*) # joepatmckenna/M@ybe1day
-(git add -A . && cat version | xargs git commit -m  && git push -f origin master) # joepatmckenna/maybe1day
